68e5040c74c895aa51a0a237a47ed33c72c390f5
sumanthghanta <sumanth96.rc@gmail.com>
2021-11-02 18:40:59 -0500

    updated

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bottleRouter = require('./routes/bottle');
 var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector')
 
 var app = express();
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bottle', bottleRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function (err, req, res, next) {
